c950b73cf643d2477161da60710b6ef8c71ff5b6
spartonia <sd_prtn@yahoo.com>
2017-05-15 18:04:15 +0200

    .gitignore updated

--- datagenerator.py
+++ datagenerator.py
@@ -11,12 +11,12 @@ Keras library. But as I needed BGR color format for fine-tuneing AlexNet I
 wrote my own little generator.
 """
 
+
 class ImageDataGenerator:
     def __init__(self, class_list, horizontal_flip=False, shuffle=False, 
-                 mean = np.array([104., 117., 124.]), scale_size=(227, 227),
-                 nb_classes = 2):
-        
-                
+                 mean=np.array([104., 117., 124.]), scale_size=(227, 227),
+                 nb_classes=3):
+
         # Init params
         self.horizontal_flip = horizontal_flip
         self.n_classes = nb_classes
@@ -24,6 +24,10 @@ class ImageDataGenerator:
         self.mean = mean
         self.scale_size = scale_size
         self.pointer = 0
+
+        self.images = []
+        self.labels = []
+        self.data_size = None
         
         self.read_class_list(class_list)
         
@@ -36,26 +40,24 @@ class ImageDataGenerator:
         """
         with open(class_list) as f:
             lines = f.readlines()
-            self.images = []
-            self.labels = []
             for l in lines:
-                items = l.split()
+                items = l.split(',')
                 self.images.append(items[0])
                 self.labels.append(int(items[1]))
             
-            #store total number of data
+            # store total number of data
             self.data_size = len(self.labels)
         
     def shuffle_data(self):
         """
         Random shuffle the images and labels
         """
-        images = self.images.copy()
-        labels = self.labels.copy()
+        images = self.images[:]
+        labels = self.labels[:]
         self.images = []
         self.labels = []
         
-        #create list of permutated index and shuffle data accoding to list
+        # create list of permutated index and shuffle data accoding to list
         idx = np.random.permutation(len(labels))
         for i in idx:
             self.images.append(images[i])
@@ -69,8 +71,7 @@ class ImageDataGenerator:
         
         if self.shuffle:
             self.shuffle_data()
-        
-    
+
     def next_batch(self, batch_size):
         """
         This function gets the next n ( = batch_size) images from the path list
@@ -80,7 +81,7 @@ class ImageDataGenerator:
         paths = self.images[self.pointer:self.pointer + batch_size]
         labels = self.labels[self.pointer:self.pointer + batch_size]
         
-        #update pointer
+        # update pointer
         self.pointer += batch_size
         
         # Read images
@@ -88,15 +89,15 @@ class ImageDataGenerator:
         for i in range(len(paths)):
             img = cv2.imread(paths[i])
             
-            #flip image at random if flag is selected
+            # flip image at random if flag is selected
             if self.horizontal_flip and np.random.random() < 0.5:
                 img = cv2.flip(img, 1)
             
-            #rescale image
+            # rescale image
             img = cv2.resize(img, (self.scale_size[0], self.scale_size[0]))
             img = img.astype(np.float32)
             
-            #subtract mean
+            # subtract mean
             img -= self.mean
                                                                  
             images[i] = img
@@ -106,5 +107,5 @@ class ImageDataGenerator:
         for i in range(len(labels)):
             one_hot_labels[i][labels[i]] = 1
 
-        #return array of images and labels
+        # return array of images and labels
         return images, one_hot_labels
